8a7cf59686ccae6ed5b0fef8447957e86fefeaa5
AvrisNox <noahd@mtu.edu>
2020-02-29 15:26:43 -0500

    No Wheel Reinvention
    I shouldn't reinvent the wheel. This commit removes my data structures that already exist in java.util. I knew they were there, but I wanted the practice. While writing this I realized that I'd want to use MY versions for everything, but I know I'll never write data structures as thoroughly as the libraries that already exist are written. Removing those and all their references.

--- src/com/avrisnox/util/graph/components/Vertex.java
+++ src/com/avrisnox/util/graph/components/Vertex.java
@@ -1,46 +1,4 @@
 package com.avrisnox.util.graph.components;
 
 public class Vertex<T> {
-	private T element;
-	private Edge<T, ?> next;
-	private Edge<T, ?> prev;
-
-	public Vertex() { }
-	public Vertex(T element){
-		this();
-		setElement(element);
-	}
-	public Vertex(Edge<T, ?> next, Edge<T, ?> prev){
-		this();
-		setNext(next);
-		setPrev(prev);
-	}
-	public Vertex(Edge<T, ?> next, Edge<T, ?> prev, T element) {
-		this(next, prev);
-		setElement(element);
-	}
-
-	public void setElement(T element) {
-		this.element = element;
-	}
-
-	public T getElement() {
-		return element;
-	}
-
-	public void setNext(Edge<T, ?> next) {
-		this.next = next;
-	}
-
-	public Edge<T, ?> getNext() {
-		return next;
-	}
-
-	public void setPrev(Edge<T, ?> prev) {
-		this.prev = prev;
-	}
-
-	public Edge<T, ?> getPrev() {
-		return prev;
-	}
 }
